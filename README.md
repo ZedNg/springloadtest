6facfc65c7c3d208ebafd93b9d1058584c8e8918
Zed Ng <zedng@WG410-Loan-MDDN142-01.local>
2021-09-27 13:17:58 +1300

    add markdown and fix CSS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# springloadtest
+# Form Validation Test (for Springload)
+
+I have made a simple form based on the criteria set out on [this link](https://github.com/springload/form-validation-problem/). I have done this site based on a Mobile First Approach as that will allow me to use that screen size as a baseline for design. I have tested this website on Chrome,  Safari, and Safari for iOS( on a iPhone SE 1st Generation)as those are the browser I have access to.
+
+The bootstrap Framework(version 5.1.1) is heavily utilised here as it is the framework I am most familiar with and it reduce the time required to build the basic structure of the site.
+
+If I have more time, I would do more testing to check for different scenarios. I would also add more helpful prompts for the user. While I was able to put the character condition on the password validation, I was unable to setup a prompt to inform the user if they have less than 8 characters. For now, I have just added a line in the input area stating "more than 8 characters" as a temporary fix. I was also unable to setup a prompt to inform users when the form is successfully validated and submitted. 
--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
   'use strict'
   // Block position for checklist for mobile
-  let smallDevice = window.matchMedia("(min-width: 800px)");
+  let smallDevice = window.matchMedia("(min-width: 780px)");
   handleDeviceChange(smallDevice);
   smallDevice.addListener(handleDeviceChange);
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // Fetch all the selector from the HTML
   let forms = document.querySelectorAll('.requires-validation')
-
   let showTiger = document.getElementById('tigerCheckbox')
-
+  let pwdCheck = document.getElementById("passwordInput")
+  //listen for the Tiger CheckBox
   showTiger.addEventListener('change', showTypeTiger);
-
-
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function(form) {
@@ -22,13 +18,9 @@
         var array = []
         var checkboxes = document.querySelectorAll('input[type=checkbox]:checked')
         var showValid = document.getElementById('selectOne')
-
-
-
         for (var i = 0; i < checkboxes.length; i++) {
           array.push(checkboxes[i].value)
         }
-
 
         if (array.length == 0) {
           for (var j = 0; j < checkBox.length; j++) {
@@ -41,22 +33,17 @@
             showValid.classList.remove("show")
           }
         }
-
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
-
-
-
         }
-        form.classList.add('was-validated')
-
+        form.classList.add('was-validated');
 
       }, false)
     })
 
+  //Show Type of Tiger textbox when tiger is pressed
   function showTypeTiger() {
-
     let showType = document.getElementById('unHide')
     if (this.checked) {
       showType.classList.add("show")
@@ -64,7 +51,6 @@
       showType.classList.remove("show")
     }
   }
-
   // Function when screenwidth is < 600px
   function handleDeviceChange(e) {
     let changeLayout = document.getElementsByClassName('form-check');
